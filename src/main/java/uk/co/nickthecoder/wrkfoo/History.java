0851ff4e881110bfb443bc2aae80153c4487d648
Nick Robinson <nickthecoder@gmail.com>
2017-03-10 12:24:05 +0000

    Prevent duplicate Moments in History.

--- src/main/java/uk/co/nickthecoder/wrkfoo/History.java
+++ src/main/java/uk/co/nickthecoder/wrkfoo/History.java
@@ -10,6 +10,7 @@ import uk.co.nickthecoder.jguifier.Parameter;
 import uk.co.nickthecoder.jguifier.Task;
 import uk.co.nickthecoder.jguifier.ValueParameter;
 import uk.co.nickthecoder.jguifier.util.Exec;
+import uk.co.nickthecoder.jguifier.util.Util;
 import uk.co.nickthecoder.wrkfoo.tool.ExecTool;
 
 public class History
@@ -24,8 +25,44 @@ public class History
         currentIndex = -1; // We don't have any Moment yet.
     }
 
+    /**
+     * Is the current state of the tool, the same as the current Moment.
+     * Used to determine if another Moment should be created by {@link #add}.
+     * 
+     * @param tool
+     *            The tool to compare with the current Moment.
+     * @return true if the Tool is the same class, and the parameters have all the same values.
+     */
+    public boolean sameAsCurrent(Tool tool)
+    {
+        if (currentIndex < 0) {
+            return false;
+        }
+
+        Moment current = history.get(currentIndex);
+        if (current.tool.getClass() != tool.getClass()) {
+            return false;
+        }
+
+        for (String paramName : current.parameterValues.keySet()) {
+            Parameter parameter = tool.getTask().findParameter(paramName);
+            if (parameter instanceof ValueParameter) {
+                ValueParameter<?> vp = (ValueParameter<?>) parameter;
+                if (!Util.equals(vp.getValue(), current.parameterValues.get(paramName))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     public void add(Tool tool)
     {
+        if (sameAsCurrent(tool)) {
+            return;
+        }
+
         while (currentIndex < history.size() - 1) {
             history.remove(history.size() - 1);
         }
@@ -37,7 +74,7 @@ public class History
             moment = new Moment(new ExecTool(new Exec("echo", "Tool prevented from rerunning")));
         }
         history.add(moment);
-        currentIndex ++;
+        currentIndex++;
     }
 
     public boolean canUndo()
@@ -100,7 +137,7 @@ public class History
         public Tool restore()
         {
             Task task = tool.getTask();
-            
+
             for (String key : parameterValues.keySet()) {
                 try {
                     Parameter parameter = task.getParameters().findParameter(key);
