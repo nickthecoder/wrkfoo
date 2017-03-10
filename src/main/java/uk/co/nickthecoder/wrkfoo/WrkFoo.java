0910c318ba1d3cc553efa2f5857dd707ae5d37fc
Nick Robinson <nickthecoder@gmail.com>
2017-03-10 23:46:58 +0000

    WrkFoo can be passed a File or a name of a TabSet.

--- src/main/java/uk/co/nickthecoder/wrkfoo/WrkFoo.java
+++ src/main/java/uk/co/nickthecoder/wrkfoo/WrkFoo.java
@@ -1,26 +1,43 @@
 package uk.co.nickthecoder.wrkfoo;
 
+import java.io.File;
+
 import javax.swing.SwingUtilities;
 
 import uk.co.nickthecoder.jguifier.FileParameter;
+import uk.co.nickthecoder.jguifier.StringParameter;
 import uk.co.nickthecoder.jguifier.Task;
 import uk.co.nickthecoder.jguifier.util.Util;
 
 public class WrkFoo extends Task
 {
-    public FileParameter tabSet = new FileParameter.Builder("tabSet")
+    public FileParameter tabSetFile = new FileParameter.Builder("tabSetFile")
         .optional().file().mustExist()
         .parameter();
+
+    public StringParameter tabSetName = new StringParameter.Builder("tabSetName")
+        .optional()
+        .parameter();
+
+    public WrkFoo()
+    {
+        addParameters(tabSetFile, tabSetName);
+    }
 
     @Override
     public void body()
     {
-        if (tabSet.getValue() == null) {
+        if (tabSetFile.getValue() != null) {
+            TabSetData.load(tabSetFile.getValue()).openMainWindow();
+
+        } else if (tabSetName.getValue() != null) {
+            File file = new File(Resources.instance.getTabsDirectory(), tabSetName.getValue());
+            TabSetData.load(file).openMainWindow();
+
+        } else {
             MainWindow mainWindow = new MainWindow();
             mainWindow.onWorkTabSets();
             mainWindow.setVisible(true);
-        } else {
-            TabSetData.load(tabSet.getValue()).openMainWindow();
         }
     }
 
