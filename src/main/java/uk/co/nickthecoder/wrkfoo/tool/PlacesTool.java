eecb8da63be62317716be2d4f325fbb386777c41
Nick Robinson <nickthecoder@gmail.com>
2017-03-28 09:29:41 +0100

    Places can now be edited.

--- src/main/java/uk/co/nickthecoder/wrkfoo/tool/PlacesTool.java
+++ src/main/java/uk/co/nickthecoder/wrkfoo/tool/PlacesTool.java
@@ -1,8 +1,11 @@
 package uk.co.nickthecoder.wrkfoo.tool;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
+import java.util.List;
 
 import javax.swing.Icon;
 
@@ -11,6 +14,7 @@ import uk.co.nickthecoder.jguifier.guiutil.Places.Place;
 import uk.co.nickthecoder.jguifier.parameter.FileParameter;
 import uk.co.nickthecoder.jguifier.parameter.StringParameter;
 import uk.co.nickthecoder.jguifier.util.Exec;
+import uk.co.nickthecoder.jguifier.util.Util;
 import uk.co.nickthecoder.wrkfoo.AbstractListTool;
 import uk.co.nickthecoder.wrkfoo.Column;
 import uk.co.nickthecoder.wrkfoo.Columns;
@@ -96,31 +100,147 @@ public class PlacesTool extends AbstractListTool<PlacesTask, Place>
         return addTask;
     }
 
+    public Task remove(Place place)
+    {
+        RemoveTask task = new RemoveTask();
+        task.file.setValue(place.file);
+        task.label.setValue(place.label);
+        return task;
+    }
+
+    public Task edit(Place place)
+    {
+        EditTask task = new EditTask();
+        task.oldFile.setDefaultValue(place.file);
+        task.file.setDefaultValue(place.file);
+        task.label.setDefaultValue(place.label);
+        return task;
+    }
+
     public class AddTask extends Task
     {
         public final FileParameter file = new FileParameter.Builder("file").fileOrDirectory()
             .value(Resources.getInstance().getHomeDirectory())
             .parameter();
 
-        public final StringParameter placeName = new StringParameter.Builder("placeName").optional().parameter();
+        public final StringParameter label = new StringParameter.Builder("label").optional().parameter();
 
         public AddTask()
         {
             super();
-            addParameters(file, placeName);
+            addParameters(file, label);
         }
 
         @Override
         public void body()
         {
             try {
-                new Exec("echo", "file://" + file.getValue().getPath(), placeName.getValue())
+                new Exec("echo", "file://" + file.getValue().getPath(), label.getValue())
                     .stdout(task.store.getValue(), true) // append to the Places file
                     .run();
             } catch (IOException e) {
                 e.printStackTrace();
             }
             PlacesTool.this.go();
+        }
+    };
+
+    public class RemoveTask extends Task
+    {
+        public final FileParameter file = new FileParameter.Builder("file").fileOrDirectory()
+            .value(Resources.getInstance().getHomeDirectory())
+            .parameter();
+
+        public final StringParameter label = new StringParameter.Builder("label").optional().parameter();
+
+        public RemoveTask()
+        {
+            super();
+            addParameters(file, label);
+        }
+
+        @Override
+        public void body()
+        {
+            List<Place> places = getTask().getResults();
+            try {
+                PrintWriter out = new PrintWriter(getTask().store.getValue());
+
+                for (Place place : places) {
+                    if (Util.equals(place.file, file.getValue())) {
+                        continue;
+                    }
+                    out.print("file://");
+                    out.print(place.file.getPath());
+                    if (!Util.empty(place.label)) {
+                        out.print(" ");
+                        out.println(place.label);
+                    } else {
+                        out.println();
+                    }
+                }
+
+                out.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    public class EditTask extends Task
+    {
+        public final FileParameter oldFile = new FileParameter.Builder("oldFile").fileOrDirectory()
+            .hide().parameter();
+
+        public final FileParameter file = new FileParameter.Builder("file").fileOrDirectory()
+            .parameter();
+
+        public final StringParameter label = new StringParameter.Builder("label").optional().parameter();
+
+        public EditTask()
+        {
+            super();
+            addParameters(oldFile, file, label);
+        }
+
+        @Override
+        public void body()
+        {
+            List<Place> places = getTask().getResults();
+            try {
+                PrintWriter out = new PrintWriter(getTask().store.getValue());
+
+                for (Place place : places) {
+
+                    if (Util.equals(place.file, oldFile.getValue())) {
+
+                        out.print("file://");
+                        out.print(file.getValue().getPath());
+                        if (!Util.empty(label.getValue())) {
+                            out.print(" ");
+                            out.println(label.getValue());
+                        } else {
+                            out.println();
+                        }
+
+                    } else {
+                        out.print("file://");
+                        out.print(place.file.getPath());
+                        if (!Util.empty(place.label)) {
+                            out.print(" ");
+                            out.println(place.label);
+                        } else {
+                            out.println();
+                        }
+                    }
+                }
+
+                out.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     };
 }
