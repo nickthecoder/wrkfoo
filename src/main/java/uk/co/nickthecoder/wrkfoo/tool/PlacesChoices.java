a25bc238ca4dc988a0b1199de881c3e3f9fa9e27
Nick Robinson <nickthecoder@gmail.com>
2017-04-04 18:42:34 +0100

    PlacesChoices rewrite. Now has two tables, the list of places files, and the list of Places.

--- src/main/java/uk/co/nickthecoder/wrkfoo/tool/PlacesChoices.java
+++ src/main/java/uk/co/nickthecoder/wrkfoo/tool/PlacesChoices.java
@@ -1,23 +1,47 @@
 package uk.co.nickthecoder.wrkfoo.tool;
 
+import java.awt.BorderLayout;
 import java.io.File;
 import java.io.PrintWriter;
 
 import javax.swing.Icon;
+import javax.swing.JSplitPane;
 
 import uk.co.nickthecoder.jguifier.Task;
+import uk.co.nickthecoder.jguifier.guiutil.Places.Place;
 import uk.co.nickthecoder.jguifier.parameter.StringParameter;
+import uk.co.nickthecoder.wrkfoo.AbstractListTool;
+import uk.co.nickthecoder.wrkfoo.Column;
+import uk.co.nickthecoder.wrkfoo.Columns;
+import uk.co.nickthecoder.wrkfoo.MergedToolPanel;
+import uk.co.nickthecoder.wrkfoo.PanelResults;
 import uk.co.nickthecoder.wrkfoo.Resources;
+import uk.co.nickthecoder.wrkfoo.SimpleTable;
+import uk.co.nickthecoder.wrkfoo.TableResults;
+import uk.co.nickthecoder.wrkfoo.ToolPanel;
+import uk.co.nickthecoder.wrkfoo.tool.PlacesChoices.PlacesChoicesResults;
+import uk.co.nickthecoder.wrkfoo.util.FileNameRenderer;
 
 /**
- * Like the {@link PlacesTool} tool, but instead of asking for a places store using a FileParameter,
- * this has a directory, where many places stores live, and you pick one from a pull down list.
+ * Lists a directory containing "places" files. The contents of one of the places files is listed in a
+ * separate table.
  */
-public class PlacesChoices extends PlacesTool
+public class PlacesChoices extends AbstractListTool<PlacesChoicesResults, PlacesChoicesTask, WrappedFile>
 {
+    private PlacesTool placesTool;
+
     public PlacesChoices()
     {
         super(new PlacesChoicesTask());
+
+        placesTool = new PlacesTool()
+        {
+            @Override
+            protected ToolPanel createToolPanel()
+            {
+                return new MergedToolPanel(PlacesChoices.this.getToolPanel());
+            }
+        };
     }
 
     @Override
@@ -33,37 +57,53 @@ public class PlacesChoices extends PlacesTool
     }
 
     @Override
-    public String getTitle()
+    public SimpleTable<WrappedFile> getTable()
     {
-        return "Places Choices";
+        return getResultsPanel().tableResults.getTable();
     }
 
-    /**
-     * Used by the '+p' option
-     * 
-     * @return A task which will add a new the places file when run.
-     */
-    public AddPlacesTask addPlacesTask()
+    @Override
+    public Columns<WrappedFile> createColumns()
     {
-        return new AddPlacesTask();
+        Columns<WrappedFile> columns = new Columns<>();
+
+        columns = new Columns<>();
+
+        columns.add(new Column<WrappedFile>(File.class, "name")
+        {
+            @Override
+            public File getValue(WrappedFile row)
+            {
+                return row.file;
+            }
+        }.sort().width(300).renderer(FileNameRenderer.instance));
+
+        return columns;
     }
 
-    /**
-     * Used by the '-p' option
-     * 
-     * @return A task which will remove the place files when run.
-     */
-    public RemovePlaceTask removePlacesTask()
+    @Override
+    protected PlacesChoicesResults createResultsPanel()
     {
-        return new RemovePlaceTask();
+        return new PlacesChoicesResults();
     }
 
-    public class AddPlacesTask extends Task
+    public void showPlaces(File file)
+    {
+        placesTool.getTask().store.setValue(file);
+        placesTool.go();
+    }
+
+    public Task createPlacesTask()
+    {
+        return new CreatePlacesTask();
+    }
+
+    public class CreatePlacesTask extends Task
     {
         public StringParameter name = new StringParameter.Builder("name")
             .parameter();
 
-        public AddPlacesTask()
+        public CreatePlacesTask()
         {
             addParameters(name);
         }
@@ -75,22 +115,31 @@ public class PlacesChoices extends PlacesTool
             // Create an empty file
             PrintWriter writer = new PrintWriter(file);
             writer.close();
-            getTask().updateChoices();
         }
     }
 
-    public class RemovePlaceTask extends Task
+    public class PlacesChoicesResults extends PanelResults
     {
-        public RemovePlaceTask()
-        {
-        }
+        TableResults<WrappedFile> tableResults;
 
-        @Override
-        public void body() throws Exception
+        TableResults<Place> placesTableResults;
+
+        public PlacesChoicesResults()
         {
-            File file = getTask().store.getValue();
-            file.delete();
-            getTask().updateChoices();
+            super(PlacesChoices.this);
+            SimpleTable<WrappedFile> table = getColumns().createTable(getTableModel());
+
+            tableResults = new TableResults<WrappedFile>(PlacesChoices.this, table);
+
+            placesTableResults = placesTool.getResultsPanel();
+
+            JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+            splitPane.setResizeWeight(0.3);
+
+            getComponent().add(splitPane, BorderLayout.CENTER);
+
+            splitPane.setTopComponent(tableResults.getComponent());
+            splitPane.setBottomComponent(placesTableResults.getComponent());
         }
     }
 }
--- src/main/java/uk/co/nickthecoder/wrkfoo/tool/PlacesChoicesTask.java
+++ src/main/java/uk/co/nickthecoder/wrkfoo/tool/PlacesChoicesTask.java
@@ -1,64 +1,42 @@
 package uk.co.nickthecoder.wrkfoo.tool;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
-import uk.co.nickthecoder.jguifier.ParameterListener;
-import uk.co.nickthecoder.jguifier.parameter.ChoiceParameter;
+import uk.co.nickthecoder.jguifier.Task;
 import uk.co.nickthecoder.jguifier.parameter.FileParameter;
-import uk.co.nickthecoder.jguifier.parameter.Parameter;
 import uk.co.nickthecoder.jguifier.util.FileLister;
+import uk.co.nickthecoder.wrkfoo.ListResults;
 
-public class PlacesChoicesTask extends PlacesTask
+public class PlacesChoicesTask extends Task implements ListResults<WrappedFile>
 {
-    public FileParameter directory = new FileParameter.Builder("directory").directory().mustExist()
-        .parameter();
+    private List<WrappedFile> results;
 
-    public ChoiceParameter<File> storeChoices = new ChoiceParameter.Builder<File>("store")
+    public FileParameter directory = new FileParameter.Builder("directory").directory().mustExist()
         .parameter();
 
     public PlacesChoicesTask()
     {
         super();
-
-        directory.addListener(new ParameterListener()
-        {
-
-            @Override
-            public void changed(Parameter source)
-            {
-                updateChoices();
-            }
-        });
-
-        storeChoices.addListener(new ParameterListener()
-        {
-            @Override
-            public void changed(Parameter source)
-            {
-                if (storeChoices.getValue() != null) {
-                    store.setValueIgnoreErrors(storeChoices.getValue());
-                }
-            }
-        });
-
-        store.visible = false;
-        addParameters(directory, storeChoices);
+        addParameters(directory);
     }
 
-    public void updateChoices()
+    @Override
+    public void body() throws Exception
     {
-        storeChoices.clearChoices();
-
-        if (directory.getValue() == null) {
-            return;
-        }
+        results = new ArrayList<>();
 
         FileLister lister = new FileLister();
-        List<File> files = lister.listFiles(directory.getValue());
 
-        for (File file : files) {
-            storeChoices.addChoice(file.getName(), file);
+        for (File file : lister.listFiles(directory.getValue())) {
+            results.add(new WrappedFile(file));
         }
+    }
+
+    @Override
+    public List<WrappedFile> getResults()
+    {
+        return results;
     }
 }
